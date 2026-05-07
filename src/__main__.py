f81c0fd1eb31b9a0e137b5c416fd5dd0865e4166
SBIX7 <bestlol01124801281@gmail.com>
2026-05-07 22:09:10 +0100

    done

--- src/__main__.py
+++ src/__main__.py
@@ -29,7 +29,7 @@ def main():
     prompts = load_prompt(args.input)
     llm = Small_LLM_Model()
     decoder = JSONConstrainedDecoder(llm, functions)
-    print(decoder.decode(prompts[0].model_dump()['prompt']))
+    print(decoder.decode(prompts[2].model_dump()['prompt']))
 
 
 if __name__ == "__main__":
--- src/decoder.py
+++ src/decoder.py
@@ -27,7 +27,7 @@ class JSONConstrainedDecoder:
             if idx not in valid_tokens_id:
                 logits[idx] = float("-inf")
 
-    def calculate_remainder(self, current_text: str, cible: str) -> str:
+    def calculate_remainder(self, current_text: str, cible: str) -> tuple:
         rest_to_write = cible
         max_size = len(cible)
         for i in range(max_size, 0, -1):
@@ -35,7 +35,7 @@ class JSONConstrainedDecoder:
                 return rest_to_write[i:], i
         return cible, 0
 
-    def _get_remainder(self, current_text: str, cibles: List[str]) -> List[str, int]:
+    def _get_remainder(self, current_text: str, cibles: List[str]) -> list:
         potential_remainders = []
         for cible in cibles:
             remainder, score = self.calculate_remainder(current_text, cible)
@@ -45,7 +45,7 @@ class JSONConstrainedDecoder:
     def decode(self, prompt: str) -> str:
         i = 0
         generated_text = "{"
-        state_machine = "KEY_NAME_SEARCH"
+        state_machine = "STATE_WRITE_NAME_KEY"
         cible = ['"name": "']
         chosen_function = None
         parametres_restants = []
@@ -56,22 +56,25 @@ class JSONConstrainedDecoder:
             ids_1d = ids_2d[0].tolist()
             logits = self.llm.get_logits_from_input_ids(ids_1d)
 
-            if state_machine == "KEY_NAME_SEARCH" and generated_text.endswith(
-                tuple(cible)
+            if (
+                state_machine == "STATE_WRITE_NAME_KEY"
+                and generated_text.endswith(tuple(cible))
             ):
-                state_machine = "VALUE_NAME_SEARCH"
+                state_machine = "STATE_WRITE_FUNC_NAME"
                 cible = [f.name + '", "' for f in self.functions]
 
-            elif state_machine == "VALUE_NAME_SEARCH" and generated_text.endswith(
-                tuple(cible)
+            elif (
+                state_machine == "STATE_WRITE_FUNC_NAME"
+                and generated_text.endswith(tuple(cible))
             ):
-                state_machine = "KEY_PARAMETERS_SEARCH"
+                state_machine = "STATE_WRITE_PARAMS_KEY"
                 cible = ['parameters": {']
 
-            elif state_machine == "KEY_PARAMETERS_SEARCH" and generated_text.endswith(
-                tuple(cible)
+            elif (
+                state_machine == "STATE_WRITE_PARAMS_KEY"
+                and generated_text.endswith(tuple(cible))
             ):
-                state_machine = "WRITE_PARAM_KEY"
+                state_machine = "STATE_WRITE_ARG_KEY"
                 match = re.search(r'"name": "(.*?)"', generated_text)
                 if match:
                     func_name = match.group(1)
@@ -86,13 +89,41 @@ class JSONConstrainedDecoder:
                         if parametres_restants:
                             cible = [parametres_restants[0]]
 
+            elif (
+                state_machine == "STATE_WRITE_ARG_KEY"
+                and generated_text.endswith(tuple(cible))
+            ):
+                state_machine = "STATE_WRITE_ARG_VALUE"
+                if chosen_function:
+                    if chosen_function.parameters.get(cible[0]) == "number":
+                        alwd = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+                        alwd += [".", "0", ",", "}", "-"]
+                        self._apply_mask(logits, alwd)
+                        best_token_id = logits.index(max(logits))
+                        best_token_str = self.llm.decode([best_token_id])
+                        generated_text += best_token_str
+
+                        print(f"Tour {i} | Etat: {state_machine} | Texte : {generated_text}")
+
+                        if i == 200:
+                            break
+                        i += 1
+
+
+                        continue
+                
+
             targets = cible
             permitted_words = self._get_remainder(generated_text, targets)
+
             if permitted_words:
                 higher_score = max([score for _, score in permitted_words])
                 permitted_words = [
-                    word for word, score in permitted_words if score == higher_score
+                    word
+                    for word, score in permitted_words
+                    if score == higher_score
                 ]
+
             self._apply_mask(logits, permitted_words)
 
             best_token_id = logits.index(max(logits))
